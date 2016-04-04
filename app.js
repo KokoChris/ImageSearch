2163e6fb0b1db79a6c64a95ba4f2534605010006
kokoChris <kokolios1986@yahoo.gr>
2016-04-04 20:30:57 +0300

    demo request for 10 results

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 var express = require('express'),
+	http 	= require('http'),
+	request = require('request'),
 	port    = process.env.PORT || 3000,
+	apiKey  = 'PtDnqJYXMyc+KnBQWMpykJmKunkbJfv2fTqOgx21a6A',
 	mongoose = require('mongoose'),
+	bodyParser = require('body-parser');
 	app  	= express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 app.get('/',function (req,res) {
 	res.send("Hello from the root route");
+});
+
+
+
+app.get('/search/:term',function(req,res) {
+	// var options = {
+
+	// } 
+    request('https://user:PtDnqJYXMyc+KnBQWMpykJmKunkbJfv2fTqOgx21a6A@api.datamarket.azure.com/Bing/Search/v1/Image?Query=%27xbox%27&$top=10&$format=JSON', function (error, response, body) {
+	   
+	    if (!error && response.statusCode == 200) {
+	    	res.json(JSON.parse(body));
+  	}
+})
 });
 
 app.listen(port,function(){
