bb43e49475c02a6a9c0700c42f6d196ccc6f9fb1
kostas <xaitakon@gmail.com>
2016-04-05 21:05:22 +0300

    req.query

--- app.js
+++ app.js
@@ -40,16 +40,12 @@ app.get('/',function (req,res) {
 
 app.get('/search/:term',function(req,res) {
     
-    var id = req.query.id;
-	
     
-    res.end("I have received the ID: " + id);
-    
+    var number = req.query.offset || 10;
+
         
 	//sample request--this is not yet dynamic :term from request should take the place of xbos,also we should have dynamic limit and take less data from the json body of the request
-    request(apiStart+apiKey+apiCode+ req.params.term + '%27&$top=10&$format=JSON', function (error, response, body) {
-        
-        
+    request(apiStart+apiKey+apiCode+ req.params.term + '%27&$top='+number+'&$format=JSON', function (error, response, body) {     
 	   
 	    if (!error && response.statusCode == 200) {
 	    	var  parsedBody =  JSON.parse(body);
